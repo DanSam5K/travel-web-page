00462bd5388f23d4280da7aeed025b17ac39cc38
Daniel Samuel <dansam5000@gmail.com>
2021-12-06 23:20:38 +0100

    festyle: navbar component and buttone elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,21 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize');
+
   return (
     <>
       <nav className="navbar">
@@ -51,6 +63,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle="'btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
