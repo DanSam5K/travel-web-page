4c9d6171acb4be1280d0a0fa8afa1520d202b40d
Daniel Samuel <dansam5000@gmail.com>
2021-12-13 23:07:54 +0100

    fix Modify Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import Button from './Button';
@@ -18,13 +18,17 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    showButton();
+  }, []);
+
   window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             TRAVEL XP
             <i className="fab fa-typo3" />
           </Link>
